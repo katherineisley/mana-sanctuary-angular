6a6a9c25ac12f58bb3c3a2a2afdeb7807c7edab4
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2023-12-12 00:52:01 +0200

    Create card designs for About Us, problem with hexagonal avatar border

--- src/app/about-us-card/about-us-card.component.ts
+++ src/app/about-us-card/about-us-card.component.ts
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class AboutUsCardComponent {
   @Input() card: any;
+
+  getBackgroundStyle() {
+    return `background: linear-gradient(#18181A, #18181A) padding-box, linear-gradient(0.395turn, ${this.card.color1}, ${this.card.color2}) border-box;`
+    // lord have mercy I CAN'T TAKE THIS ANYMORE
+  }
 }
