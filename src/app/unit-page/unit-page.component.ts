d70e7c06a11ebbfceb040334fd48d481745d0bee
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2023-12-15 22:26:33 +0200

    Create prototype unit (Umi) data structure, unfinished

--- src/app/unit-page/unit-page.component.ts
+++ src/app/unit-page/unit-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-unit-page',
   templateUrl: './unit-page.component.html',
   styleUrls: ['./unit-page.component.scss']
 })
-export class UnitPageComponent {
+export class UnitPageComponent /* implements OnInit */ {
+  // name!: string;
+  // unit: any;
+  
+  // constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
+  // ngOnInit() {
+  //   this.name = this.route.snapshot.paramMap.get('name')!;
+  //   this.http.get('../../assets/' + this.name + '.json').subscribe(data => {
+  //     this.unit = data[this.name];
+  //   });
+  // }
 }
