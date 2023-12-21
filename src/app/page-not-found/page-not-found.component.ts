e1a0ade227e563b4d9935c7e4ef91521e81ba639
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2023-12-22 00:37:12 +0200

    Viewport height sorcery for page to be 100% of window due to lack of content

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent {
 
+export class PageNotFoundComponent implements AfterViewInit {
+  constructor() { }
+
+  adjustHeight(): void {
+    const headerHeight = $('.header').height();
+    const footerHeight = $('.footer').height();
+    const windowHeight = $(window).height();
+    if (windowHeight && headerHeight && footerHeight) {
+      const newHeight = windowHeight - headerHeight - footerHeight - 2; // the -2 comes from the border of the footer
+      console.log(newHeight);
+      $('app-page-not-found').css('height', newHeight + 'px');
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.adjustHeight();
+    $(window).on('resize', () => this.adjustHeight());
+  }
+
+  ngOnDestroy(): void {
+    $(window).off('resize');
+  }
 }
