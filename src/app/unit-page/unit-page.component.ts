2c26346b465f946a60e7afd32a238b81ce8e9ba2
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2024-01-04 23:36:22 +0200

    Improve the application func of element-dependant page coloring

--- src/app/unit-page/unit-page.component.ts
+++ src/app/unit-page/unit-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import * as $ from 'jquery';
@@ -12,6 +12,7 @@ import * as $ from 'jquery';
 export class UnitPageComponent implements OnInit, AfterViewInit {
   slug!: string;
   unit: any = {};
+  activeElement: any = null;
   
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
@@ -19,35 +20,28 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
     this.slug = this.route.snapshot.paramMap.get('name')!;
     this.http.get<any>("../../assets/json/simulacra-data.json").subscribe(data => {
       this.unit = data.find((unit: any) => unit.slug === this.slug);
-      console.log(this.unit);
-      switch (this.unit.element) {
-        case "element_physical":
-        case "element_physicalflame":
-          $('.tabs .tab').css('border', '1px solid #CF9B14');
-          $('.tab.active').css('background-color', '#CF9B14');
-          break;
-        case "element_flame":
-        case "element_flamephysical":
-          $('.tabs .tab').css('border', '1px solid #E74412');
-          $('.tab.active').css('background-color', '#E74412');
-          break;
-        case "element_frost":
-        case "element_frostvolt":
-          $('.tabs .tab').css('border', '1px solid #3498DB');
-          $('.tab.active').css('background-color', '#3498DB');
-          break;
-        case "element_volt":
-        case "element_voltfrost":
-          $('.tabs .tab').css('border', '1px solid #8C7ED0');
-          $('.tab.active').css('background-color', '#8C7ED0');
-          break;
-      }
-    });
+      // console.log(this.unit);
+      const elementColors: {[key: string]: string} = {
+        "element_physical": '#CF9B14',
+        "element_physicalflame": '#CF9B14',
+        "element_flame": '#E74412',
+        "element_flamephysical": '#E74412',
+        "element_frost": '#3498DB',
+        "element_frostvolt": '#3498DB',
+        "element_volt": '#8C7ED0',
+        "element_voltfrost": '#8C7ED0'
+      };
+      document.documentElement.style.setProperty('--element-color', elementColors[this.unit.element]);
+    }, error => console.error(error));
+  }
+
+  setActiveElement(element: any) {
+    this.activeElement = element;
   }
 
   ngAfterViewInit() {
+    $('.tabs .tab:first-child').addClass('active');
     $('.tabs .tab').click(function () {
-      console.log('clicked');
       $(this).addClass('active').siblings().removeClass('active');
     });
   }
