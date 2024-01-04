63ca2312bf594bcc41cd3009d4fd63d96759116d
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2024-01-04 23:42:42 +0200

    🧹Cleanup of previous commit, remove unused imports and extra ".active"

--- src/app/unit-page/unit-page.component.ts
+++ src/app/unit-page/unit-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import * as $ from 'jquery';
