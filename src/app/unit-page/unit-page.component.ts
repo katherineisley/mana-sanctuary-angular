7c113e16dbdb1103f3a2a026bf090033ee8bda80
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2024-01-05 18:24:28 +0200

    Add Profile tab info selection, sliding underline animation

--- src/app/unit-page/unit-page.component.ts
+++ src/app/unit-page/unit-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import * as $ from 'jquery';
@@ -12,7 +12,7 @@ import * as $ from 'jquery';
 export class UnitPageComponent implements OnInit, AfterViewInit {
   slug!: string;
   unit: any = {};
-  activeElement: any = null;
+  activeTab: any = null;
   
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
@@ -21,7 +21,7 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
     this.http.get<any>("../../assets/json/simulacra-data.json").subscribe(data => {
       this.unit = data.find((unit: any) => unit.slug === this.slug);
       // console.log(this.unit);
-      const elementColors: {[key: string]: string} = {
+      const elementColors: {[key: string]: string} = { // this sets the page color depending on the element
         "element_physical": '#CF9B14',
         "element_physicalflame": '#CF9B14',
         "element_flame": '#E74412',
@@ -35,14 +35,36 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
     }, error => console.error(error));
   }
 
-  setActiveElement(element: any) {
-    this.activeElement = element;
+  setActiveTab(element: any) { 
+    this.activeTab = element;
   }
 
+  setActiveInfo(target: EventTarget | null) {
+    if (target instanceof HTMLElement) {
+      const rect = target.getBoundingClientRect();
+      const underline = document.querySelector('.underline') as HTMLElement;
+      const containerRect = (underline.parentElement as HTMLElement).getBoundingClientRect();
+      underline.style.left = `${rect.left - containerRect.left - 8}px`; // offset the width extension
+      underline.style.width = `${rect.width + 16}px`; // make it a bit prettier
+    }
+  }
+
+  @ViewChildren('info') infos!: QueryList<ElementRef>;
+
   ngAfterViewInit() {
-    $('.tabs .tab:first-child').addClass('active');
+    this.setActiveInfo(this.infos.first.nativeElement); // make the underline appear under "Advancements" on page load
+    setTimeout(() => {
+      $('.underline').css('transition', 'left 0.3s ease, width 0.3s ease');
+    }, 1); // shit solution so it doesnt get applied mid-render
+
+    $('.tabs .tab:first-child').addClass('active'); // make "Profile" tab active on page load
     $('.tabs .tab').click(function () {
       $(this).addClass('active').siblings().removeClass('active');
+      if (!$('.tabs .tab[data-tab="profile"]').hasClass('active')) { // i doubt this solution will be permanent or at least not this ugly
+        $('.profile-part').hide();
+      } else {
+        $('.profile-part').show();
+      }
     });
   }
 }
