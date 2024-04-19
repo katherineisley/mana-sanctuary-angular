d13a757b07fbab3a6ab209cf83bcc5b27a5df384
Neptune <Kevin.Klemm@tptlive.ee>
2024-04-19 11:14:35 +0300

    Fix dynamic content height after multitude of header and footer changes + minor improvements, minor adjustments and cleanup of unit page

--- src/app/dynamic-content-height.directive.ts
+++ src/app/dynamic-content-height.directive.ts
@@ -1,37 +1,37 @@
-import { Directive, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDynamicContentHeight]'
 })
 
-export class DynamicContentHeightDirective implements AfterViewInit {
+export class DynamicContentHeightDirective {
   private resizeListener!: Function;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngAfterViewInit(): void {
-    this.adjustHeight();
     this.resizeListener = this.renderer.listen('window', 'resize', () => {
       this.renderer.setStyle(this.el.nativeElement, 'height', 'unset'); // fix potential problem where the explicitly set height might no longer be appropriate due to changes
       this.adjustHeight();
     });
   }
 
+  @HostListener('window:load', ['$event'])
+  onWindowLoad(event: any): void {
+    this.adjustHeight();
+  }
+
   private adjustHeight(): void {
-    const headerHeight = $('.header').height() || 0;
-    const footerHeight = $('.footer').height() || 0;
+    const headerHeight = $('.header').outerHeight(true) || 0;
+    const footerHeight = $('.footer').outerHeight(true) || 0;
     const contentHeight = $(this.el.nativeElement).outerHeight(true) || 0;
     const windowHeight = $(window).height() || 0;
-    // console.log("Header:", headerHeight);
-    // console.log("Footer:", footerHeight);
-    // console.log("Content:", contentHeight);
-    // console.log("Combined:", headerHeight + footerHeight + contentHeight);
-    // console.log("Window:", windowHeight);
     
     if (headerHeight + footerHeight + contentHeight < windowHeight) {
-      // console.log("adjusting");
-      const newHeight = windowHeight - headerHeight - footerHeight - 2 - this.getBotTopMargin(); // the -2 comes from the border of the footer
-      // console.log(newHeight);
+      console.log("adjusting");
+      const newHeight = windowHeight - headerHeight - footerHeight - this.getBotTopMargin();
+      console.log(windowHeight, headerHeight, footerHeight, this.getBotTopMargin());
+      console.log(newHeight);
       this.renderer.setStyle(this.el.nativeElement, 'height', `${newHeight}px`);
     }
   }
