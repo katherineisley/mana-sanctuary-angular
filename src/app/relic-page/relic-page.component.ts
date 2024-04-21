8487bca4ab98ac762d0ce4e6ea3dc1c89a3569f3
katherineisley <143841680+katherineisley@users.noreply.github.com>
2024-04-21 01:18:28 +0100

    Relic page additions

--- src/app/relic-page/relic-page.component.ts
+++ src/app/relic-page/relic-page.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-relic-page',
   templateUrl: './relic-page.component.html',
   styleUrls: ['./relic-page.component.scss']
 })
-export class RelicPageComponent {
 
+
+
+export class RelicPageComponent implements OnInit, AfterViewInit {
+  slug!: string;
+  relic: any = {};
+  activeTab: any = null;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
+
+
+  ngOnInit() {
+    this.slug = this.route.snapshot.paramMap.get('name')!;
+    this.http.get<any>("assets/json/relics-data.json").subscribe(data => {
+      this.relic = data.find((relic: any) => relic.slug === this.slug);
+
+    }, error => console.error(error));
+  }
+
+  setActiveTab(element: any) {
+    this.activeTab = element;
+  }
+
+  ngAfterViewInit() {
+
+    $('.tabs .tab:first-child').addClass('active'); // make "Profile" tab active on page load
+
+    $('.tabs .tab').click(function () {
+      var tabName = $(this).data('tab');
+      $(this).addClass('active').siblings().removeClass('active');
+
+      $('.profile-part, .video-part, .guide-part').hide(); // Hide all parts initially
+
+      switch (tabName) {
+        case 'profile':
+          $('.profile-part').show();
+          break;
+        case 'video':
+          $('.video-part').show();
+          break;
+        case 'guide':
+          $('.guide-part').show();
+          break;
+      }
+    });
+  }
 }
