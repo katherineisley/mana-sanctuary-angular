2e2b55c2831f390ab28e2c3304fac59eb11a8740
katherineisley <143841680+katherineisley@users.noreply.github.com>
2024-05-19 00:10:09 +0100

    Finished Weapon Materials

--- src/app/unit-page/unit-page.component.ts
+++ src/app/unit-page/unit-page.component.ts
@@ -18,11 +18,12 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
   activeTab: any = null;
   activeInfo: string = 'Advancements';
   sliderValues: number[] = [0, 200];
-  materialsColour: any=[];
+  materialsColour: any = [];
   totalMaterialsArray: [string, number][] = [];
   startvalue: number = 0;
   endvalue: number = 200;
   materialsElement!: string;
+  
   frostMappings: { [key: string]: string } = {
     'elementalCore_A': 'icecore_A',
     'upgradeA_A': 'acidproofglaze_A',
@@ -47,7 +48,6 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
     'upgradeB_C': 'boosterframe_C',
   };
 
-
   physicalMappings: { [key: string]: string } = {
     'elementalCore_A': 'rockcore_A',
     'upgradeA_A': 'nanocoating_A',
@@ -60,7 +60,6 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
     'upgradeB_C': 'boosterframe_C',
   };
 
-
   flameMappings: { [key: string]: string } = {
     'elementalCore_A': 'firecore_A',
     'upgradeA_A': 'acidproofglaze_A',
@@ -71,6 +70,21 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
     'elementalCore_C': 'firecore_C',
     'upgradeA_C': 'acidproofglaze_C',
     'upgradeB_C': 'nanofiberframe_C',
+  };
+
+  alteredMappings: { [key: string]: string } = {
+    'upgradeA_A': 'nanocoating_A',
+    'upgradeB_A': 'acidproofglaze_A',
+    'upgradeC_A': 'boosterframe_A',
+    'upgradeD_A': 'nanofiberframe_A',
+    'upgradeA_B': 'nanocoating_B',
+    'upgradeB_B': 'acidproofglaze_B',
+    'upgradeC_B': 'boosterframe_B',
+    'upgradeD_B': 'nanofiberframe_B',
+    'upgradeA_C': 'nanocoating_C',
+    'upgradeB_C': 'acidproofglaze_B',
+    'upgradeC_C': 'boosterframe_C',
+    'upgradeD_C': 'nanofiberframe_C',
   };
 
   constructor(
@@ -173,16 +187,20 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
       "elementalCore_A": "#4286DC",
       "upgradeA_A": "#4286DC",
       "upgradeB_A": "#4286DC",
+      "upgradeC_A": "#4286DC",
+      "upgradeD_A": "#4286DC",
       "elementalCore_B": "#986BB5",
       "upgradeA_B": "#986BB5",
       "upgradeB_B": "#986BB5",
+      "upgradeC_B": "#986BB5",
+      "upgradeD_B": "#986BB5",
       "elementalCore_C": "#D99F44",
       "upgradeA_C": "#D99F44",
       "upgradeB_C": "#D99F44",
+      "upgradeC_C": "#D99F44",
+      "upgradeD_C": "#D99F44",
     };
-
-
-
+    let augmentAmountIndex = 0; 
     for (let i = 0; i < this.upgradeData.upgrade.length; i++) {
       const upgrade = this.upgradeData.upgrade[i];
       if (upgrade.level >= startLevel && upgrade.level <= endLevel) {
@@ -191,28 +209,27 @@ export class UnitPageComponent implements OnInit, AfterViewInit {
         totalMaterials['augmentGold'] = (totalMaterials['augmentGold'] || 0) + upgrade.augmentGold;
         // Loop through augmentM  aterials array and count each material
         upgrade.augmentMaterials.forEach((material: string) => { // Specify type string for material
-          totalMaterials[material] = (totalMaterials[material] || 0) + upgrade.augmentAmount;
+
+          totalMaterials[material] = (totalMaterials[material] || 0) + upgrade.augmentAmount[augmentAmountIndex++];
         });
+        if (augmentAmountIndex >= upgrade.augmentAmount.length) {
+          augmentAmountIndex = 0;
+        }
       }
     }
 
-
-
     // Convert the object into an array of {material: total} pairs
     this.totalMaterialsArray = Object.entries(totalMaterials);
-
-
-// Iterate over each array and create an entry value if the first element matches a specific value
-for (let i = 0; i < this.totalMaterialsArray.length; i++) {
-  let firstElement = this.totalMaterialsArray[i][0];
-  if (thirdValueMappings.hasOwnProperty(firstElement)) {
-    // If a mapping exists for the first element, add the corresponding entry value to the materialsColour array
-    let entryValue = thirdValueMappings[firstElement];
-    this.materialsColour.push(entryValue);
-  }
-}
-
-console.log(this.materialsColour)
+    this.materialsColour = [];
+    // Iterate over each array and create an entry value if the first element matches a specific value
+    for (let i = 0; i < this.totalMaterialsArray.length; i++) {
+      let firstElement = this.totalMaterialsArray[i][0];
+      if (thirdValueMappings.hasOwnProperty(firstElement)) {
+        // If a mapping exists for the first element, add the corresponding entry value to the materialsColour array
+        let entryValue = thirdValueMappings[firstElement];
+        this.materialsColour.push(entryValue);
+      }
+    }
     // Iterate over the array of arrays and modify the inner arrays
     for (let i = 0; i < this.totalMaterialsArray.length; i++) {
       const innerArray = this.totalMaterialsArray[i];
@@ -238,9 +255,11 @@ console.log(this.materialsColour)
           innerArray[0] = this.physicalMappings[firstElement];
         }
       }
-
-
-
+      if (this.materialsElement == 'element_altered') {
+        if (this.alteredMappings.hasOwnProperty(firstElement)) {
+          innerArray[0] = this.alteredMappings[firstElement];
+        }
+      }
     }
     // Trigger change detection
     this.cdr.detectChanges();
