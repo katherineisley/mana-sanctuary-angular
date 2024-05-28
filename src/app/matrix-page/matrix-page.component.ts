1f6549c6db9d0cdbda1c50ba3eefdc181a6c8537
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2024-05-28 22:04:00 +0300

    Make small changes to Relic Page, add link card to Unit Page on Matrix Page

--- src/app/matrix-page/matrix-page.component.ts
+++ src/app/matrix-page/matrix-page.component.ts
@@ -13,6 +13,8 @@ import * as $ from 'jquery';
 export class MatrixPageComponent implements OnInit {
   slug!: string;
   matrix: any = {};
+  specificUnit: any = {};
+  validLinks: number = 0;
   activeTab: any = null;
 
   constructor(
@@ -25,12 +27,18 @@ export class MatrixPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.validLinks = 0;
     const data = this.route.snapshot.data['data'];
     const matricesData = data.matrices;
+    this.specificUnit = data.simulacra.find((unit: any) => unit.slug === this.route.snapshot.paramMap.get('name')!);
+
+    if (this.specificUnit) {
+      this.validLinks++;
+    }
 
     this.slug = this.route.snapshot.paramMap.get('name')!;
     this.matrix = matricesData.find((matrix: any) => matrix.slug === this.slug);
-
+    
     if(this.matrix) {
       const element = this.matrix.element;
       console.log(element);
@@ -44,16 +52,17 @@ export class MatrixPageComponent implements OnInit {
         "element_frostvolt": '#3498DB',
         "element_volt": '#8C7ED0',
         "element_voltfrost": '#8C7ED0',
-        "element_altered": '#0EA667'
+        "element_altered": '#0EA667',
+        "element_all": '#F5F5F5'
       };
 
       if (element.length > 1) {
-        document.documentElement.style.setProperty('--use-gradient-border', 'true');
+        // document.documentElement.style.setProperty('--use-gradient-border', 'true');
         document.documentElement.style.setProperty('--element-gradient-color1', elementColors[element[0]]);
         document.documentElement.style.setProperty('--element-gradient-color2', elementColors[element[1]]);
         document.documentElement.style.removeProperty('--element-color');
       } else {
-        document.documentElement.style.setProperty('--use-gradient-border', 'false');
+        // document.documentElement.style.setProperty('--use-gradient-border', 'false');
         document.documentElement.style.setProperty('--element-color', elementColors[element[0]]);
         document.documentElement.style.removeProperty('--element-gradient-color1');
         document.documentElement.style.removeProperty('--element-gradient-color2');
